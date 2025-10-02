718ff9763535fbf9bfcd50444ebc37889af408f3
Melkamu Woldemariam <melkamu2@yahoo.com>
2025-10-02 08:45:45 -0400

    Additional css styling done

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,7 +1,7 @@
 export default function Person({name, birth_year, eye_color}) {
     return (
       <div className="card1">
-        <h3>Name: {name}</h3>
+        <h1>Name: {name}</h1>
         <h2>Birth Year: {birth_year}</h2>
         <h2>Eye Color: {eye_color}</h2>
       </div>
--- src/components/Planet.jsx
+++ src/components/Planet.jsx
@@ -1,7 +1,7 @@
 export default function Person({name, rotation_period, climate}) {
     return (
       <div className="card3">
-        <h3>Name: {name}</h3>
+        <h1>Name: {name}</h1>
         <h2>Rotation Period: {rotation_period}</h2>
         <h2>Climate: {climate}</h2>
       </div>
